b6161519ee12d3e3b8af23328709fb570f4e80a8
LiamAspell <liamaspell123@gmail.com>
2023-04-17 21:03:52 +0100

    ..

--- src/pages/generic.js
+++ src/pages/generic.js
@@ -2,22 +2,17 @@ import React from 'react'
 import { useState } from 'react';
 import Helmet from 'react-helmet'
 import {FaGithub, FaLinkedin, FaHackerrank} from 'react-icons/fa';
-import pic11 from '../assets/images/pic11.jpg'
 import gif from '../assets/images/Command Pattern.gif'
 import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
 import BannerLanding from '../components/BannerLanding'
 import Python from 'react-icons/fa'
-
 import pic08 from '../assets/images/pic08.png'
 import pic09 from '../assets/images/pic09.png'
 import pic10 from '../assets/images/pic10.png'
-
+import pic11 from '../assets/images/pic11.jpg'
 import pic12 from '../assets/images/Jenkins.png'
 import pic13 from '../assets/images/MachineLearning.jpg'
-
-
 
 const Generic = (props) => (
     <Layout>
@@ -35,6 +30,42 @@ const Generic = (props) => (
                         <h1>Projects</h1>
                     </header>
 
+                    
+                    <h2 id="content">Interactive Python Browser Games representing Bebras Tasks</h2>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                        <div>
+                            <p>This was my Final Year capstone project, completed over a 6 month phase.</p>
+                            <p>Paragraph 2</p>
+                            <p>Paragraph 3</p>
+                            <ul className="icons">
+                                <a href="https://github.com/LiamAspell"><FaGithub  color="white" size="3rem"/></a>
+                            </ul>
+                        </div>
+                        {/*<span><img src={pic11} height="600px" width="600px" /></span>*/}
+                    </div>
+
+                    
+                    <h2 id="content">Spotify Unwrapped, A music programming project</h2>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                        <div>
+                            <p>This was a solo project completed as part of the music programming module in Final Year.</p>
+                            <p>Paragraph 2</p>
+                            <p>Paragraph 3</p>
+                            <ul className="icons">
+                                <a href="https://github.com/LiamAspell"><FaGithub  color="white" size="3rem"/></a>
+                            </ul>
+                        </div>
+                        {/*<span><img src={pic11} height="600px" width="600px" /></span>*/}
+                    </div>
+
                     <h2 id="content">SVG Code Generator implementing C# and .NET Framework</h2>
                     <div style={{
                         display: 'flex',
@@ -50,47 +81,28 @@ const Generic = (props) => (
                             </ul>
                         </div>
                         {/*<span><img src={pic11} height="600px" width="600px" /></span>*/}
-
                     </div>
-
-                    <h2 id="content">SVG Code Generator implementing C# and .NET Framework</h2>
-
-                   
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
-                        <div>
-                            <p>I completed this project over the course of a semester as part of the Software Design Patterns module.</p>
-                            <p>It is a command line based application, which takes letters as arguments, that generates svg code for a variety of different shapes, which can be coloured or styled accordingly. It is basically a minimized version of MS Paint for SVG shapes.</p>
-                            <p>During this project I learned how to use design patterns such as the memento pattern, factory pattern among others, to implement different functions, such as undo and redo.</p>
-                            <ul className="icons">
-                                <a href="https://github.com/LiamAspell"><FaGithub  color="white" size="3rem"/></a>
-                            </ul>                        
-                        </div>
-                        {/*<span><img src={pic11} height="600px" width="600px" /></span>*/}
-                    </div>
+                  
+                    <h2 id="content">Personal Portfolio Website implementing ReactJS and GatsbyJS</h2>
+                    <p>This project is the Website that you are currently visiting! There was a few iterations of this project before it, but this is the most recent iteration and is the best looking, fastest performing and the one with the most features so far.</p>
+                    <p>In a way it represents a journey of learning to code, from the very basics of html and css, front end frameworks, some javascript operations, use of react hooks and use of GatsbyJS.</p>
+                    <p>The website with hosted with the aid of netlify, which is a very basic hosting service, in order to give it a location on the web.</p>
+                    <p>Overall, the project was very easy to setup and style and I will be making adding further features to it in the future.</p>
+                    <ul className="icons">
+                        <a href="https://github.com/LiamAspell"><FaGithub  color="white" size="3rem"/></a>
+                    </ul>
 
                     <h2 id="content">Golf Tracker</h2>
-
-                    <p>Donec eget  ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
-
-                    <ul className="icons">
-                        <a href="https://github.com/LiamAspell"><FaGithub  color="white" size="3rem"/></a>
-                    </ul>
-
-
-                    <h2 id="content">Personal Portfolio Website implementing ReactJS and GatsbyJS</h2>
-                    <p>Donec eget  ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+                    <p>This is a project that I built as part of a team for a module in third year of University. It ran for three months overall and the objective was to collaborate to build an application of some type.</p>
+                    <p>We decided to build an application to record and track golf scores, so that an individual user could track their statistics over time, and get a more accurate view of their scores in order to recognise their strengths and weaknesses.</p>
+                    <p>I played in active role in the production of this project. Initially, I designed a navigation bar to link all of the different pages together. After this I worked on a profile page, and helped with the production of some of the other pages, before finally documenting the project and performing some testing.</p>
 
                     <ul className="icons">
                         <a href="https://github.com/LiamAspell"><FaGithub  color="white" size="3rem"/></a>
                     </ul>
+
+
+                    
 
                     {/*Insert the Gif here at later stage*/}
 
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -133,9 +133,9 @@ const Landing = (props) => (
             <section id="one">
                 <div className="inner">
                     <header className="major">
-                        <h1>Skills</h1>
+                        <h1>Certifications</h1>
                     </header>
-                    <p>This section will document the technologies I have worked with within my time at University and Work. I will talk about my experience with the technologies as well as .</p>
+
                 </div>
             </section>
             <section id="two" className="spotlights">
@@ -148,7 +148,7 @@ const Landing = (props) => (
                             <header className="major">
                                 <h3>Learning Selenium</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>Selenium is a technology I first learnt in Third Year of University, in the form of a team project, where we created a website and found it necessary to run some walkthrough tests with the aid of Selenium. Since then I attained this certification, and have wrote some selenium test scripts for this website to ensure all of the pages load successfully, and buttons, links work as expected.</p>
                            
                         </div>
                     </div>
@@ -162,7 +162,7 @@ const Landing = (props) => (
                             <header className="major">
                                 <h3>Learning Linux Command Line</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>This was a very enjoyable certification which acted mostly as a refresher. I used the linux command line a good bit in University, but since I took this course and applied the various different commands on a daily basis, inside docker, the windows subsystem for linux and within virtual machines, my knowledge of the command line continues to develop.</p>
                         </div>
                     </div>
                 </section>
@@ -177,7 +177,7 @@ const Landing = (props) => (
                             <header className="major">
                                 <h3>Learning Docker</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>This course gave me a comprenhensive introduction into how to use Docker. I learned how to build Dockerfiles which I could then use to build images, which I could then use to build containers, and how to pass files into these containers which I could use in my testing. I also learnt how to use vim as part of this certification, for creating, editing files efficiently.</p>
                            
                         </div>
                     </div>
@@ -191,7 +191,7 @@ const Landing = (props) => (
                             <header className="major">
                                 <h3>Learning Python</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>Due to my prior knowledge of Object Orientated Programming methodoligies, Python was a language I could pick up quite easily, with the aid of this course. It taught me a little bit about the background of python, some of the attributes, such as the importance of indentation. This course helped prepare me for some of the work writing scripts I would go on to do in Oracle</p>
                         </div>
                     </div>
                 </section>
@@ -206,24 +206,12 @@ const Landing = (props) => (
                             <header className="major">
                                 <h3>Learning Git</h3>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>Version Control is one of the most important skills in software development. I gained a cert in Git from Linkedin Learning during my first few weeks at Oracle. I now use Git frequently through github, and am able to use the commands more comfortably with practice over time. This helps me to keep a better track over the development of my personal projects.</p>
                            
                         </div>
                     </div>
                 </section>
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={WorkingRemotely} alt="" width='90px'/>
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Working Remotely</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                        </div>
-                    </div>
-                </section>
+                
             </section>
 
         </div>
